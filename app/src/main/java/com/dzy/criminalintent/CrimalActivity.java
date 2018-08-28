55d6f6851fd6408ab72308ed60c43995dd37f5ba
dongziyu <dongziyu@daojia-inc.com>
2018-08-28 15:53:51 +0800

    创建UI fragment

---
+++ app/src/main/java/com/dzy/criminalintent/CrimalActivity.java
@@ -0,0 +1,13 @@
+package com.dzy.criminalintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrimalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crimal);
+    }
+}
---
+++ app/src/main/java/com/dzy/criminalintent/Crime.java
@@ -0,0 +1,51 @@
+package com.dzy.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * created by dzy on 2018/8/27
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID(); //产生一个随机唯一ID值
+        mDate = new Date();
+    }
+
+    public UUID getmId() {
+        return mId;
+    }
+
+    public void setmId(UUID mId) {
+        this.mId = mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean ismSolved() {
+        return mSolved;
+    }
+
+    public void setmSolved(boolean mSolved) {
+        this.mSolved = mSolved;
+    }
+}
